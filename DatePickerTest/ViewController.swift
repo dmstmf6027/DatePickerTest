040ef2f9bf7117def9d6f1bd391b85b739ebaef9
201631053 <hubot@githhub@com>
2018-04-11 16:04:59 +0900

    add datePicker

--- DatePickerTest/ViewController.swift
+++ DatePickerTest/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let timeSelector : Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
 
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeDatePicker(_ sender: Any) {
+        let datePickerView = sender
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from: (datePickerView as AnyObject).date)
+}
+    @objc func updateTime() {
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+        formatter.dateFormat = "hh:mm aaa"
+    }
 
 }
-
